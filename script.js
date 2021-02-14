975a4d1817abc53ac157e1c2b6bcbd1e59e40c03
talisondev <talison.developer@gmail.com>
2021-02-14 14:26:35 -0300

    version 2.0

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
+let display = document.querySelector("input#display");
+
 function limpar() {
-    document.querySelector("input#display").value = "";
+    display.value = "";
 }
 
 function limparOne() {
-    let display = document.querySelector("input#display").value;
-    let getLastValue = display.substr(-1);
-    let removeLastValue = display.replace(getLastValue, "");
-    document.querySelector("#display").value = removeLastValue;
+    let getLastValue = display.value.substr(-1);
+    let removeLastValue = display.value.replace(getLastValue, "");
+    display.value = removeLastValue;
 }
 
 function valor(num) {
-    document.querySelector("#display").value += num;
+    display.value += num;
 }
 
 function raiz() {
-    let valor = parseInt(document.querySelector("#display").value);
+    let valor = parseInt(display.value);
     let result = Math.sqrt(valor);
-    result
-        ? (document.querySelector("#display").value = result)
-        : (document.querySelector("#display").value = "");
-    // document.querySelector("#display").value = result;
+    result ? (display.value = result) : (display.value = "");
 }
 
 function result() {
     let resultado = 0;
     resultado = display.value;
-    resultado
-        ? (document.querySelector("#display").value = eval(resultado))
-        : (document.querySelector("#display").value = "");
-    // document.querySelector("#display").value = eval(resultado);
+    resultado ? (display.value = eval(resultado)) : (display.value = "");
 }
